94d3f72fd8319263881b349744cdca53b3767568
Daniel Zainyllin <splitebr@mail.ru>
2022-09-22 16:12:03 +0500

    fixed fatal error with work in branches

--- src/main.js
+++
@@ -1,8 +0,0 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import router from './router'
-import store from './store'
-import './styles/style.css'
-import './styles/media-phone.css'
-
-createApp(App).use(store).use(router).mount('#app')
