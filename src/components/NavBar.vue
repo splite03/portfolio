919c31cff1ca6b5a844c5ef3ec0f9a1f1051b986
Daniel Zainyllin <splitebr@mail.ru>
2022-09-22 15:40:25 +0500

    finished work with drop menu, also with @media

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -7,15 +7,22 @@
                 </div>
                 <div class="navbar-right">
                     <span class="navbar-link-with-underline">
-                        <a href="" @click.prevent="scrollToAbout()">Обо мне</a>
+                        <a href="" class="navbar-link" @click.prevent="scrollToAbout()">Обо мне</a>
                         <span class="navbar-underline"></span>
                     </span>
                     <span class="navbar-link-with-underline">
-                        <a href="" @click.prevent="drop()">Проекты</a>
+                        <a href="" class="navbar-link" @click.prevent="drop()">Проекты</a>
                         <span class="navbar-underline"></span>
+                        <span class="navbar-drop-hider">
+                            <span class="navbar-drop">
+                                <router-link to="/">Notes</router-link>
+                                <router-link to="/">Tasks</router-link>
+                                <router-link to="/">Toggle</router-link>
+                            </span>
+                        </span>
                     </span>
                     <span class="navbar-link-with-underline">
-                        <a href="" @click.prevent="">Контакты</a>
+                        <a href="" class="navbar-link" @click.prevent="">Контакты</a>
                         <span class="navbar-underline"></span>
                     </span>
                 </div>
@@ -28,11 +35,6 @@
             </div>
         </div>
     </div>
-    <span class="navbar-drop">
-        <router-link to="/">Notes</router-link>
-        <router-link to="/">Tasks</router-link>
-        <router-link to="/">Toggle</router-link>
-    </span>
 </template>
 <script>
 export default {
@@ -58,21 +60,35 @@ export default {
     .navbar-drop{
         display: flex;
         width: 150px;
-        position: fixed;
-        top: -150px;
-        right: 440px;
+        position: absolute;
+        top: -100px;
         background: rgb(92, 92, 92);
         flex-direction: column;
-        align-items: center;
+        align-items: stretch;
         justify-content: space-around;
         z-index: 0;
         transition: top .6s ease-in-out, opacity 1s;;
     }
     .navbar-drop a{
-        padding: 20px;
+        text-align: center;
+        padding: 20px 20px;
         font-size: 20px;
     }
+    .navbar-drop a:hover{
+        background: rgb(71, 71, 71);
+        color: rgb(142, 142, 142);
+    }
+    .navbar-drop-hider{
+        display: flex;
+        height: 150px;
+        width: 120%;
+        overflow: hidden;
+        position: absolute;
+        top:46px;
+        align-content: center;
+        justify-content: center;
+    }
     .drop{
-        top: 60px;
+        top: 0;
     }
 </style>
